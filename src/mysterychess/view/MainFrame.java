94dd5c13a088ba3e25d999ce51d5aa5e4ccfd67c
huytin <huytin@gmail.com>
2011-08-04 14:50:16 +0000

    problem with SVN: Re-add MainFrame.java

---
+++ src/mysterychess/view/MainFrame.java
@@ -0,0 +1,73 @@
+/*
+ * MainFrame.java
+ *
+ * Created on Mar 10, 2011, 8:51:45 PM
+ */
+package mysterychess.view;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.WindowEvent;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+
+import mysterychess.model.Match;
+import mysterychess.network.Chatter;
+
+/**
+ * 
+ * @author Administrator
+ */
+public class MainFrame extends javax.swing.JFrame {
+
+    private Match match;
+    private JPanel chatPanel;
+    private ChessPanel chessPanel;
+    private final Chatter chatter;
+
+    /** Creates new form MainFrame */
+    public MainFrame(Match m, Chatter chatter, String title) {
+        super(title);
+        this.match = m;
+        this.chatter = chatter;
+        initComponents();
+        pack();
+    }
+
+    private void initComponents() {
+        JPanel contentPane = (JPanel) getContentPane();
+        contentPane.setLayout(new BorderLayout());
+        chatPanel = new ChatPanel(chatter);
+
+        JSplitPane splitPane = new JSplitPane();
+        contentPane.add(splitPane, java.awt.BorderLayout.CENTER);
+        chessPanel = new ChessPanel(match);
+        splitPane.setLeftComponent(chessPanel);
+        splitPane.setRightComponent(chatPanel);
+
+        this.setPreferredSize(new Dimension(730, 600));
+        splitPane.setDividerLocation(550);
+        splitPane.setOneTouchExpandable(true);
+        splitPane.setResizeWeight(0.80);
+    }
+
+    @Override
+    protected void processWindowEvent(WindowEvent e) {
+        if (e.getID() == WindowEvent.WINDOW_CLOSING) {
+            super.processWindowEvent(e);
+            match.shutdown(true);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE,
+                        null, ex);
+            }
+            System.exit(0);
+        } else {
+            super.processWindowEvent(e);
+        }
+    }
+}
